44a8f4c122fb2ec13e5f546f9b070ce2370986db
camilozg <camilozamora1@gmail.com>
2024-10-10 12:17:03 -0500

    modify database env variable

--- django_project/settings.py
+++ django_project/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+    'default': dj_database_url.config(default=os.getenv('DATABASE_PUBLIC_URL')),
 }
 
 
